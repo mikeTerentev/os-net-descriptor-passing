8c7c216cbe1935b7eb575dc0a770923fbc310809
mikeTerentev <terentyevmuy@gmail.com>
2019-06-08 23:55:41 +0300

    first

--- cmake-build-debug/CMakeFiles/common_utils.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcommon_utils.pdb"
-  "libcommon_utils.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/common_utils.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
